98e51bb9cac1e5c4297a7044349da8151b521a56
Golan Asraf <golanasraf77@gmail.com>
2025-10-02 09:08:36 +0300

    Refactor BoardIndex component: update sidebar to include buttons for navigation

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -1,7 +1,12 @@
 export function BoardIndex() {
     return (
         <section className="board-index full">
-            <section className="board-sidebar">Board sidebar</section>
+            <section className="board-sidebar">
+                <button className="btn-sidebar">Board</button>
+                <button className="btn-sidebar">Templates</button>
+                <button className="btn-sidebar">Home</button>
+            </section>
+
             <section className="board-main">Board main</section>
         </section>
     )
