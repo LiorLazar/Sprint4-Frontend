eb434223906d5caa095552a236c07d4d1b9dd61b
Lior Lazar <liorlazar3@gmail.com>
2025-10-10 14:46:37 +0300

    Add Star Logic && Naming Logic

--- src/cmps/BoardDetails/BoardHeader.jsx
+++ src/cmps/BoardDetails/BoardHeader.jsx
@@ -1,16 +1,29 @@
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
 import edenImg from '../../assets/img/Eden Avgi.png'
 import golanImg from '../../assets/img/Golan Asraf.png'
 import liorImg from '../../assets/img/Lior Lazar.png'
 
+import { icons } from '../SvgIcons'
+
 export function BoardHeader() {
+    const { boardId } = useParams()
+    const boards = useSelector(storeState => storeState.boardModule.boards)
+    const currentBoard = boards.find(board => board._id === boardId)
+    const starred = currentBoard?.isStarred || false
+
     return (
         <section className="board-header-container">
-            <span className="board-name">Work-Management</span>
+            <span className="board-name">{currentBoard.title}</span>
             <div className="collaborators flex">
                 <img src={edenImg} alt="Eden Avgi" className="collaborator-img" />
                 <img src={golanImg} alt="Golan Asraf" className="collaborator-img" />
                 <img src={liorImg} alt="Lior Lazar" className="collaborator-img" />
             </div>
+            <span className={starred ? 'starred' : 'not-starred'}>
+                {starred ? icons.starFilled : icons.star}
+            </span>
         </section>
     )
 }
