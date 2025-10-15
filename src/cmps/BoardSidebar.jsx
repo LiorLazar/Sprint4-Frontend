f096ff1ac5569364b3bd88ae3b635425a6309e00
Golan Asraf <golanasraf77@gmail.com>
2025-10-15 03:12:45 +0300

    Update boardindex style and svg

--- src/cmps/BoardSidebar.jsx
+++ src/cmps/BoardSidebar.jsx
@@ -1,10 +1,10 @@
+import { icons } from "./SvgIcons"
+
 export function BoardSidebar() {
     return (
         <section className="board-sidebar">
             <button className="btn-sidebar">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M3 3h8v8H3V3zm10 0h8v8h-8V3zM3 13h8v8H3v-8zm10 0h8v8h-8v-8z"/>
-                </svg>
+                <span>{icons.treloIcon}</span>
                 Boards
             </button>
             <button className="btn-sidebar">
