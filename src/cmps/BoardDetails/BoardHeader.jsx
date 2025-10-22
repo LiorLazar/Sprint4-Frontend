6098156b5a6a965023a8898b522ca9433d17de9d
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-10-22 14:28:56 +0300

    Fix Board Name updating after accessing through URL

--- src/cmps/BoardDetails/BoardHeader.jsx
+++ src/cmps/BoardDetails/BoardHeader.jsx
@@ -1,17 +1,28 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 import edenImg from '../../assets/img/Eden Avgi.png'
 import golanImg from '../../assets/img/Golan Asraf.png'
 import liorImg from '../../assets/img/Lior Lazar.png'
 
 import { icons } from '../SvgIcons'
+import { loadBoards } from '../../store/actions/board.actions'
+
 
 export function BoardHeader() {
     const { boardId } = useParams()
+    const dispatch = useDispatch()
     const boards = useSelector(storeState => storeState.boardModule.boards)
     const currentBoard = boards.find(board => board._id === boardId)
     const starred = currentBoard?.isStarred || false
+
+
+    useEffect(() => {
+        if (!boards || boards.length === 0) {
+            loadBoards()
+        }
+    }, [boards])
 
     if (!currentBoard) {
         return (
